f517294e1379be900c93948c86ea2f0e181439dd
Mkra31 <121699565+Mkra31@users.noreply.github.com>
2023-04-23 15:17:23 -0600

    Add files via upload

---
+++ elementos.js
@@ -0,0 +1,28 @@
+class Persona {
+    constructor(nombre, apellido, edad, cedula) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.cedula = cedula;
+    }
+  }
+  
+  class Animal {
+    constructor(nombre, genero, raza) {
+      this.nombre = nombre;
+      this.genero = genero;
+      this.raza = raza;
+    }
+  }
+  
+  class Veterinaria {
+    constructor(nombre, direccion, telefono) {
+      this.nombre = nombre;
+      this.direccion = direccion;
+      this.telefono = telefono;
+    }
+  }
+  
+
+
+
